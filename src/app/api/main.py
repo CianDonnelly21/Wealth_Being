ef2c8e2ab044454e841457329d471369efab7dc3
Cian Donnelly <b00160418@mytudublin.ie>
2026-02-19 16:04:09 +0000

    cors issue

--- src/app/api/main.py
+++ src/app/api/main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=["https://wealthbeing.vercel.app"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
