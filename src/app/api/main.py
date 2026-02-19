a7e738a4bc8bc5f9a10207c2dc3c6674bdaa3e67
Cian Donnelly <b00160418@mytudublin.ie>
2026-02-19 13:06:24 +0000

    Fixed the blocked link

--- src/app/api/main.py
+++ src/app/api/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000,"
-                   "https://wealth-being-eight.vercel.app"],  # your frontend
+                   "https://wealthbeing.vercel.app"],  # your frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
